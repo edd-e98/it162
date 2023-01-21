e76280963dd871ad9dfa3653d781eb2b98877310
Eddie <87498041+edd-e98@users.noreply.github.com>
2023-01-21 14:28:32 -0800

    Initial commit with my it 162 directory all set up

---
+++ it162/index.php
@@ -0,0 +1,24 @@
+<?php include './includes/header.php'; ?>
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome!</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="" />
+ <img src="images/phone.jpg" class="phone" alt="" />
+ <h3>Hi there, my name is Eddie!</h3>
+ <p>I'm an aspiring web developer located in Seattle, WA. I began my program about a year ago, hoping to become a front-end developer. After taking some courses, I've recently become interested in back-end, database design and server administration.</p>
+ <p>Outside of school, I work part-time for a small, local pottery store. Helping them re-design their website was a large part of my decision to make web development my career! We had to work with a template provided by the point-of-sales system we use, and for the most part, I was unable to edit the HTML of the page-- it was a fun challenge. You can see the end result <a href="https://www.paintthetown.studio/">here.</a></p>
+ <p>I spend my spare time making art, playing DnD, and organizing drag shows with my friends. Check out the website I made for them last quarter <a href="https://www.edd-e.net/big/">here.</a></p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <p>"Her breast is fit for pearls, <br> But I was not a "Diver" - <br> Her brow is fit for thornes <br> But I have not a crest. <br> Her heart is fit for <em>home</em>- <br> I - a Sparrow - build there <br> Sweet of twigs and twine <br> My perennial nest."</p>
+ <p><i>--Emily Dickinson</i></p>
+</aside>
+<!-- END RIGHT COL -->
+
+<?php include './includes/footer.php'; ?>
